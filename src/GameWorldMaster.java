4dc6d92ff4eba068ce7c4d5eb2f33c89ad2633a3
RLongo <RLongo@DESKTOP-LSF3M3E>
2019-11-18 14:31:26 -0500

    1.1

--- src/GameWorldMaster.java
+++ src/GameWorldMaster.java
@@ -1,6 +1,11 @@
 
 public class GameWorldMaster {
-
+	
+	//Testing some changes for GitHub push
+	int testVar;
+	int testVar2;
+	int testVar3;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
